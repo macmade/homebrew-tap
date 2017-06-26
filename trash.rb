70ba17a7c6f90f99277283291c44f1b44366484c
macmade <macmade@xs-labs.com>
2017-06-26 23:49:11 +0200

    Formula for trash

---
+++ trash.rb
@@ -0,0 +1,16 @@
+class Trash < Formula
+  desc "A command-line tool for macOS, written in Swift, that moves files to the current user's trash folder."
+  head "https://github.com/macmade/trash", :using => :git, :branch => :master
+  
+  depends_on :xcode => 8.0
+  
+  def install
+    xcodebuild "SDKROOT=", "SYMROOT=build"
+    bin.install "build/Release/trash"
+  end
+  
+  test do
+    system "touch", "foo.txt"
+    system "#{bin}/trash", "foo.txt"
+  end
+end
