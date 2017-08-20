d3f63b806b9c55bc551cc6e2a4a3d0dba896de7b
macmade <macmade@xs-labs.com>
2017-08-20 06:46:21 +0200

    xcode-coveralls

--- xcode-coveralls.rb
+++ xcode-coveralls.rb
@@ -3,6 +3,7 @@ class XcodeCoveralls < Formula
   homepage "https://macmade.github.io/xcode-coveralls"
   url "https://github.com/macmade/xcode-coveralls/archive/0.1.4.tar.gz"
   sha256 "cb112cf384b97dfc5103656b9399a5c210fe8df200b9ee7506b2c6838a027f48"
+  head "https://github.com/macmade/xcode-coveralls", :using => :git, :branch => :master
   
   depends_on :xcode => 6.0
   
