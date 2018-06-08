44d2b4cb87fca5acfe4e10e8c62c4e1b0c6aea52
macmade <macmade@xs-labs.com>
2018-06-08 22:26:09 +0200

    xcode-coveralls

--- xcode-coveralls.rb
+++ xcode-coveralls.rb
@@ -1,8 +1,8 @@
 class XcodeCoveralls < Formula
   desc "A tool to upload code coverage from Xcode to coveralls.io."
   homepage "https://macmade.github.io/xcode-coveralls"
-  url "https://github.com/macmade/xcode-coveralls/archive/0.1.4.tar.gz"
-  sha256 "cb112cf384b97dfc5103656b9399a5c210fe8df200b9ee7506b2c6838a027f48"
+  url "https://github.com/macmade/xcode-coveralls/archive/0.1.5.tar.gz"
+  sha256 "cb4e39014a5fa43a2d02304196e4b537cbe659a3152227755b4f52abbb0e5adb"
   head "https://github.com/macmade/xcode-coveralls", :using => :git, :branch => :master
   
   depends_on :xcode => 6.0
