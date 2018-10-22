bc9d073e2b22be863727154230b8c3c5ba694760
macmade <macmade@xs-labs.com>
2018-10-23 01:51:18 +0200

    git-branch-status

--- git-branch-status.rb
+++ git-branch-status.rb
@@ -2,14 +2,14 @@ class GitBranchStatus < Formula
   desc "Branch status monitoring utility for Git."
   head "https://github.com/macmade/git-branch-status", :using => :git, :branch => "master"
   
-  depends_on :xcode => 8.0
+  depends_on :xcode => 10.0
   
   def install
-    make
-    bin.install "build/git-branch-status"
+    xcodebuild "SDKROOT=", "SYMROOT=build"
+    bin.install "build/Release/git-branch-status"
   end
   
   test do
-    system "#{bin}/git-branch-status", "-h"
+    system "#{bin}/git-branch-status", "--help"
   end
 end
