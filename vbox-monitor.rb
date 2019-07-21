b90a6236dd6cdf59766e98b0acf475363321e79a
macmade <macmade@xs-labs.com>
2019-07-21 19:06:52 +0200

    Added a formula for vbox-monitor

---
+++ vbox-monitor.rb
@@ -0,0 +1,15 @@
+class VboxMonitor < Formula
+  desc "Monitoring/Debug tool for VirtualBox."
+  head "https://github.com/macmade/vbox-monitor", :using => :git, :branch => "master"
+  
+  depends_on :xcode => "10.0"
+  
+  def install
+    xcodebuild "SDKROOT=", "SYMROOT=build"
+    bin.install "build/Release/vbox-monitor"
+  end
+  
+  test do
+    system "#{bin}/vbox-monitor"
+  end
+end
