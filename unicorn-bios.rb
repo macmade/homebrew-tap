db1d6fd56499e9084c02f763ddefcd68a9a625e1
macmade <macmade@xs-labs.com>
2019-07-24 22:16:12 +0200

    Added a formula for unicorn-bios

---
+++ unicorn-bios.rb
@@ -0,0 +1,15 @@
+class UnicornBios < Formula
+  desc "Basic BIOS emulator for Unicorn Engine."
+  head "https://github.com/macmade/unicorn-bios", :using => :git, :branch => "master"
+  
+  depends_on :xcode => "10.0"
+  
+  def install
+    xcodebuild "SDKROOT=", "SYMROOT=build"
+    bin.install "build/Release/unicorn-bios"
+  end
+  
+  test do
+    system "#{bin}/unicorn-bios"
+  end
+end
