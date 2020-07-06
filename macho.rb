24f7c6ad5ad5d2cdd6910938bb686776a5c51042
macmade <macmade@xs-labs.com>
2020-07-06 23:15:09 +0200

    macho

---
+++ macho.rb
@@ -0,0 +1,15 @@
+class Macho < Formula
+  desc "Mach-O file parser."
+  head "https://github.com/macmade/macho", :using => :git, :branch => "master"
+  
+  depends_on :xcode => "10.0"
+  
+  def install
+    xcodebuild "SDKROOT=", "SYMROOT=build"
+    bin.install "build/Release/macho"
+  end
+  
+  test do
+    system "#{bin}/macho"
+  end
+end
