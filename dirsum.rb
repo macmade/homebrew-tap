17866ce5f4030aa770fc2fa42b1f2c65aadc85ba
macmade <macmade@xs-labs.com>
2024-09-26 10:34:56 +0200

    feat: dirsum 1.0.1

--- dirsum.rb
+++ dirsum.rb
@@ -1,8 +1,8 @@
 class Dirsum < Formula
   desc "A tool to display a SHA-265 hash of each file in a directory."
   homepage "https://github.com/macmade/dirsum"
-  url "https://github.com/macmade/dirsum/releases/download/1.0.0/dirsum.tar.gz"
-  sha256 "fd0381f3a6ef5b93ffea750ecd2007005d9aca87d8752d3f1ffc0be19a63a5d6"
+  url "https://github.com/macmade/dirsum/releases/download/1.0.1/dirsum.tar.gz"
+  sha256 "9c22230e22c834504cc6667a366e8692b0e0af09d91f5831f8567d957f112f94"
   head "https://github.com/macmade/dirsum", :using => :git, :branch => "main"
   
   depends_on :xcode => "10.0"
