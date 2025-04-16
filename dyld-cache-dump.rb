d31f9383675140a02a1768348a122b80aa41edbb
macmade <macmade@xs-labs.com>
2025-04-16 12:57:04 +0200

    feat: dyld-cache-dump build

--- dyld-cache-dump.rb
+++ dyld-cache-dump.rb
@@ -1,11 +1,13 @@
 class DyldCacheDump < Formula
   desc "A macOS command-line tool to dump the contents of dyld shared cache files."
   head "https://github.com/macmade/dyld-cache-dump", :using => :git, :branch => "main"
+  homepage "https://github.com/macmade/dyld-cache-dump"
+  url "https://github.com/macmade/dyld-cache-dump/releases/download/1.0.0/dyld-cache-dump.tar.gz"
+  sha256 "b836ec46a57e4b225b6aade2b67be9d762c0b0fec8b7b2acb5e977c45af16277"
   
   depends_on :xcode => "10.0"
   
   def install
-    xcodebuild "SDKROOT=", "SYMROOT=build", "CODE_SIGN_IDENTITY=", "CODE_SIGNING_REQUIRED=NO", "CODE_SIGNING_ALLOWED=NO"
-    bin.install "build/Release/dyld-cache-dump"
+    bin.install "dyld-cache-dump"
   end
 end
