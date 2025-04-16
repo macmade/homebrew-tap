2ab26c5a23f81361ffbaef5e4cdcaec83d653066
macmade <macmade@xs-labs.com>
2025-04-16 11:25:50 +0200

    feat: macho build

--- macho.rb
+++ macho.rb
@@ -1,12 +1,14 @@
 class Macho < Formula
   desc "Mach-O file parser."
   head "https://github.com/macmade/macho", :using => :git, :branch => "main"
+  homepage "https://github.com/macmade/macho"
+  url "https://github.com/macmade/macho/releases/download/1.0.0/macho.tar.gz"
+  sha256 "ba5e9f653b9390e155f6f01f702e84cec4a3b8fadd823cc55cce47156b4662a5"
   
   depends_on :xcode => "10.0"
   
   def install
-    xcodebuild "SDKROOT=", "SYMROOT=build", "CODE_SIGN_IDENTITY=", "CODE_SIGNING_REQUIRED=NO", "CODE_SIGNING_ALLOWED=NO"
-    bin.install "build/Release/macho"
+    bin.install "macho"
   end
   
   test do
