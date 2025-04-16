44dfb3739f1c3b615eebc8725f898224c8971813
macmade <macmade@xs-labs.com>
2025-04-16 11:20:43 +0200

    fix: find-string build

--- find-string.rb
+++ find-string.rb
@@ -1,12 +1,14 @@
 class FindString < Formula
   desc "Search strings in executables."
   head "https://github.com/macmade/find-string", :using => :git, :branch => "main"
+  homepage "https://github.com/macmade/find-string"
+  url "https://github.com/macmade/find-string/releases/download/1.1.0/find-string.tar.gz"
+  sha256 "04338ac4efc3a881e389974d70b71c7e3c0ff6a1b79c61b675011633935d07c6"
   
   depends_on :xcode => "10.0"
   
   def install
-    xcodebuild "SDKROOT=", "SYMROOT=build"
-    bin.install "build/Release/find-string"
+    bin.install "find-string"
   end
   
   test do
