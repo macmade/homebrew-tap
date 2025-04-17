b4dd2a7f7b840884969e2d8643d430127064fa8b
macmade <macmade@xs-labs.com>
2025-04-17 11:07:13 +0200

    feat: find-string update

--- find-string.rb
+++ find-string.rb
@@ -2,8 +2,8 @@ class FindString < Formula
   desc "Search strings in executables."
   head "https://github.com/macmade/find-string", :using => :git, :branch => "main"
   homepage "https://github.com/macmade/find-string"
-  url "https://github.com/macmade/find-string/releases/download/1.1.0/find-string.tar.gz"
-  sha256 "04338ac4efc3a881e389974d70b71c7e3c0ff6a1b79c61b675011633935d07c6"
+  url "https://github.com/macmade/find-string/releases/download/1.2.0/find-string.tar.gz"
+  sha256 "6caf89bbb15f3bfd1ee2a721b5ef83dd4f1ea1747af41a34f8f31bbe1670e042"
   
   depends_on :xcode => "10.0"
   
