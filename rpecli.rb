6fdf82ea8bcce319b297edcefd1a0df7ed37780f
macmade <macmade@xs-labs.com>
2025-04-17 11:29:20 +0200

    fix: Fixed rpecli url

--- rpecli.rb
+++ rpecli.rb
@@ -3,7 +3,7 @@ class Rpecli < Formula
   head "https://github.com/N0fix/rpecli", :using => :git, :branch => "master"
   homepage "https://github.com/N0fix/rpecli"
   version "1.0.0"
-  url "https://github.com/macmade/homebrew-tap/bin/rpecli.tar.gz"
+  url "https://github.com/macmade/homebrew-tap/raw/main/bin/rpecli.tar.gz"
   sha256 "55349f0906a2589459633a41495ec13000f79e8dc4c53b4996b0963bcd348bd1"
   
   depends_on :xcode => "10.0"
